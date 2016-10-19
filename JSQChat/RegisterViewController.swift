f77d54db17e4bf2167b49b241ebd07c77cd76771
Sriram Rajendran <sriramrajendrantkp@yahoo.in>
2016-10-19 12:14:46 +0530

    Part-6  Registering the newUser in to the Backend

--- JSQChat/RegisterViewController.swift
+++ JSQChat/RegisterViewController.swift
@@ -15,14 +15,25 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var userNameTxtFld: UITextField!
     
     
+    var backendShrdInstance = Backendless.sharedInstance()
+    var newUserObj: BackendlessUser?
     
     
+    var emailObj: String?
+    var userNameObj: String?
+    var passwordObj: String?
+    var avatarImgObj: UIImage?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        newUserObj = BackendlessUser()
+   
+        
+        // end of viewDidLoad
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,19 +44,111 @@ class RegisterViewController: UIViewController {
 
     
     @IBAction func btnRegister(sender: UIButton) {
+        
+        if  emailTxtFld.text != "" && passwordTxtFld.text != "" && userNameTxtFld.text != ""
+        {
+         emailObj = emailTxtFld.text
+        passwordObj = passwordTxtFld.text
+            userNameObj = userNameTxtFld.text
+            
+            // callBack  registerToBckendless
+            
+            registerToBckendless(self.emailObj!, userNamePrm: self.userNameObj!, passwdPrm: self.passwordObj!, avatarImagePrm:self.avatarImgObj)
+        }else{
+            
+            //Alert to user
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.displayAlert("Missing Field(s)", MessageTxt: "Username, Email and Password requiered")
+                
+            })
+
+        }
+//    end of btnRegister
+     }
+    
+    
+    //MARK: Backendless registration func
+    
+    func registerToBckendless(emailPrm: String, userNamePrm: String, passwdPrm: String, avatarImagePrm: UIImage?)  {
+        
+        if avatarImagePrm == nil {
+            
+            newUserObj?.setProperty("Avatar", object: "")
+                 }
+        
+        newUserObj?.email = emailPrm
+        newUserObj?.password = passwdPrm
+        newUserObj?.name = userNamePrm
+        
+        //register the newUser
+        backendShrdInstance.userService.registering(newUserObj, response: { (registeredUser : BackendlessUser!) in
+            
+            //callBack loginUser
+            self.loginUser(self.emailObj!, usernameLoginPrm: self.userNameObj!, passwdLoginPrm: self.passwordObj!)
+            
+            //empty the textFields
+             self.userNameTxtFld.text = ""
+            self.emailTxtFld.text = ""
+            self.passwordTxtFld.text = ""
+            
+            
+            
+        }) { (fault: Fault!) in
+            
+            print("Error in registering newUser\(fault)")
+        }
+        
+    }
+    
+    //MARK: Login func
+    func loginUser (emailLoginPrm: String, usernameLoginPrm: String, passwdLoginPrm:String  ) {
+        
+        backendShrdInstance.userService.login(emailObj, password: passwordObj
+            , response: { (users: BackendlessUser!) in
+                
+                
+                
+                
+        }) { (fault: Fault!) in
+            
+            print("Server error in loginUser \(fault)")
+            
+        }
+        
+        
     }
     
     
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+   //MARK: UIAlert
+func displayAlert(titleMsg: String, MessageTxt: String) {
+    
+    let alert = UIAlertController(title: titleMsg , message: MessageTxt , preferredStyle: .Alert)
+    
+    let saveAction = UIAlertAction(title: "Ok", style: .Default) {
+      (action: UIAlertAction) -> Void in
+      
     }
-    */
+let cancelAction = UIAlertAction(title: "Cancel", style: .Default){
+      (action: UIAlertAction) -> Void in
+    }
 
+    alert.addAction(saveAction)
+alert.addAction(cancelAction)
+    alert.view.setNeedsLayout()
+    
+    presentViewController(alert, animated: true, completion: nil)
+    
+  }
+    
+    
+    
+    
+    
+    
+    
+//MARK: end of RegisterViewController
 }
